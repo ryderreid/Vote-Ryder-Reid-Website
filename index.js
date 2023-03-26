d4b4506c592ef8425af1240678c011493e250e1c
ryderreid <shadowjreid@gmail.com>
2023-03-26 15:07:04 +1100

    Version 4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ---
+
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
